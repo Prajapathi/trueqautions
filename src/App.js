e445a55118ed5797cff885e3559a1d3be346c121
Syam Kumar <yadavallisyam1@gmail.com>
2023-07-06 20:33:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [listData, setListData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [radio, setRadio] = useState(0)
+
+  // let newArray = listData
+
+  // const [renderList, setRenderList] = useState(listData)
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setListData(json));
+  }, []);
+
+
+  const eventHandler = e => {
+
+    if (e.target.value !== "") {
+      setSearch(e.target.value)
+    }
+  }
+
+  const radioClick = e => {
+    setRadio(e.target.id)
+  }
+
+  // console.log(newArray);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        <input name="name" id="name" width='100px' height='100px' onChange={e => eventHandler(e)} />
+        <label name='asc'>ASC</label>
+        <input id="asc" type="radio" name='order' number={'asc'} onClick={e => radioClick(e)} checked={radio === 'asc'} />
+        <label name='desc'>DESC</label>
+        <input id="desc" type="radio" name='order' number={'desc'} onClick={e => radioClick(e)} checked={radio === 'desc'} />
+      </div>
+      {listData.filter(data => {
+        return data.title.toLowerCase().includes(search.toLowerCase())
+      }).sort(
+        (a, b) => {
+          if (radio === 'asc') {
+            if (a.title > b.title) {
+              return 1
+            }
+            else {
+              return -1
+            }
+          }
+          else {
+            if (a.title < b.title) {
+              return 1
+            }
+            else {
+              return -1
+            }
+          }
+        }
+      ).map((data) => {
+        return <div>
+          <img src={data.image} width='100px' heigh='100px' alt={data.title} />
+          <p>{data.title}</p>
+        </div>
+      })
+      }
+      <div>
 
-export default App;
+      </div>
+    </div>
+  )
+};
+
